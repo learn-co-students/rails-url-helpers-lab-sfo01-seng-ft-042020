e78a22e0789054db0b53a2483870d91a5688765c
tharveybrown <tharveybrown@gmail.com>
2020-05-14 10:28:18 -0700

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,3 +1,4 @@
+
 class StudentsController < ApplicationController
   before_action :set_student, only: :show
   
@@ -6,7 +7,16 @@ class StudentsController < ApplicationController
   end
 
   def show
+    @student = set_student
   end
+
+  def activate
+    @student = set_student
+    @student.active = !@student.active
+    @student.save
+    redirect_to student_path(@student)
+  end
+
 
   private
 
