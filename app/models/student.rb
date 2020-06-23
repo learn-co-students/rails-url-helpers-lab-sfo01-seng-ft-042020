3264d8fc073c8413f28e4c0ba871dab40873d86b
Tisdale511 <tisdalefry@gmail.com>
2020-06-23 13:02:35 -0700

    Done.

--- app/models/student.rb
+++ app/models/student.rb
@@ -2,4 +2,20 @@ class Student < ActiveRecord::Base
   def to_s
     self.first_name + " " + self.last_name
   end
+
+  def active?
+    self.active ? self.active : !self.active
+  end
+
+  # def activate_student_path
+  #   if !self.active
+  #     self.active = "Inactive"
+  #   else
+  #     self.active = "Active"
+  #     byebug
+  #   end
+
+  # end
+ 
+
 end
